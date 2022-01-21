65ef40d0e5692a10f67ef99579c52cbee5cf6dd0
JoksuH <haarlaj@gmail.com>
2022-01-21 11:50:22 +0200

    Added a basic datacard for book info fetched from google API

---
+++ src/components/addbook/BookDataCard.tsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import { Button, Row, Col, Image, Typography, Layout, Rate } from 'antd'
+
+const textStyle = {marginBottom: '3vh'}
+const imgStyle = { width: '20vw' }
+const layoutStyle = { width: '50vw', margin: 'auto', marginTop: '10vh', border: '1px solid black', borderRadius: '10px', padding: '20px', backgroundColor: 'white' }
+
+interface data {
+    BookData: bookData
+}
+
+interface bookData {
+  title: String
+  description: String
+  imageLinks: imagelinks | undefined
+  authors: String[]
+}
+
+interface imagelinks {
+  thumbnail: string
+}
+
+const { Title, Text } = Typography
+const { Sider, Content } = Layout
+
+const BookDataCard: React.FC<data> = ({BookData}) =>  {
+
+  return (
+    <div>
+      {BookData && (
+        <Layout style={layoutStyle}>
+            {BookData.imageLinks && 
+          <Sider style={{backgroundColor: 'white'}}>
+            <Image height="100%" width="100%" src={BookData.imageLinks.thumbnail} preview={false} />
+          </Sider>
+          }
+          <Content>
+            <Row justify="center" style={textStyle} gutter={32}>
+              <Col span={12}>
+                Title: <Title>{BookData.title}</Title>
+              </Col>
+              <Col span={12}>
+                Author:<Title>{BookData.authors[0]}</Title>
+              </Col>
+            </Row>
+            <Row justify="center" style={textStyle} gutter={12}>
+              <Col span={24}>
+                <Text italic>{BookData.description}</Text>
+              </Col>
+            </Row>
+            <Row justify="center" >
+              <Col span={24}>
+                <Text strong>Rate it: </Text>
+              </Col>
+            </Row>
+            <Row justify="center" style={textStyle} gutter={32}>
+              <Col span={24}>
+                <Rate allowHalf defaultValue={2.5} />
+              </Col>
+            </Row>
+            <Row justify="center" style={textStyle} gutter={32}>
+              <Col span={12}>
+                <Button>Add to Library</Button>
+              </Col>
+              <Col span={12}>
+                <Button>Add to WishList</Button>
+              </Col>
+            </Row>
+          </Content>
+        </Layout>
+      )}
+    </div>
+  )
+}
+
+export default BookDataCard
--- src/components/addbook/Searchbooks.tsx
+++ src/components/addbook/Searchbooks.tsx
@@ -1,8 +1,25 @@
 import React, { useState } from 'react'
-import { Input, Button } from 'antd'
+import { Input, Button, Row, Col, Image, Typography, Layout, Rate } from 'antd'
+import BookDataCard from './BookDataCard'
 
-const SearchBook = () => {
+const textStyle = {marginBottom: '3vh'}
+const imgStyle = { width: '20vw' }
+const layoutStyle = { width: '50vw', margin: 'auto', marginTop: '10vh', border: '1px solid black', borderRadius: '10px', padding: '20px', backgroundColor: 'white' }
+
+interface bookData {
+  title: String
+  description: String
+  imageLinks: imagelinks
+  authors: String[]
+}
+
+interface imagelinks {
+  thumbnail: string
+}
+
+const SearchBook: React.FC = () => {
   const [ISBN, SetISBN] = useState<String | undefined>(undefined)
+  const [BookData, SetBookData] = useState<bookData | undefined>(undefined)
 
   const handleTyping = (event: React.ChangeEvent<HTMLInputElement>) => {
     SetISBN(event.target.value)
@@ -17,8 +34,8 @@ const SearchBook = () => {
       fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${ISBN}`).then((response) => {
         response.json().then((json) => {
           if (json['items']) {
-            let bookData = json['items'][0]
-            console.log(bookData)
+            let bookData = json['items'][0]['volumeInfo']
+            SetBookData(bookData)
           } else alert('No Books Found With The Current ISBN')
         })
       })
@@ -29,6 +46,9 @@ const SearchBook = () => {
     <div>
       <Input placeholder="search book title" defaultValue={'0140278737'} onChange={handleTyping} onPaste={handlePaste} />
       <Button onClick={fetchData}>Fetch</Button>
+      {BookData && (
+       <BookDataCard BookData={BookData}/>
+      )}
     </div>
   )
 }
